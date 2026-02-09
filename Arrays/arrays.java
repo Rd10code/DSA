2144a0d6df9d4d086a124b7e703041407bf5696e
Rohit Deore <deorerohit2004@gmail.com>
2026-02-09 12:59:17 +0530

    change the name of the Array folder

---
+++ Arrays/arrays.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class arrays{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+
+        int marks[] = new int[20]; 
+
+        System.out.print(marks.length);
+        // System.out.print("Enter physics marks : ");
+        // marks[0]=sc.nextInt();
+        // System.out.print("Enter math marks : ");
+        // marks[1]=sc.nextInt();
+        // System.out.print("Enter Chemistry marks : ");
+        // marks[2]=sc.nextInt();
+        // marks[2]=marks[2]+1;
+
+        // System.out.print("Your marks are");
+    
+        // System.out.println("Phycsic : "+marks[0] );
+        // System.out.println("Math : "+marks[1] ); 
+        // System.out.println("Chemistry : "+marks[2] );
+
+        // int percentage = (marks[0]+marks[1]+marks[2])/3;
+        // System.out.println("Percentage is "+percentage);
+        sc.close();
+
+        
+        
+    }
+}
