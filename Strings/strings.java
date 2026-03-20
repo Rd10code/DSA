fcddf85a966d44a4c01b43c3477e97e2b5b97535
Rohit Deore <deorerohit2004@gmail.com>
2026-03-20 23:40:25 +0530

    Done Palimdrome code

--- Strings/strings.java
+++ Strings/strings.java
@@ -1,26 +1,34 @@
 import java.util.*;
 public class strings{
-    public static void main(String args[]){
-        int arr[]={'a','b','c'};
-        String str ="abcd";
-        String str2= new String("xyz");
 
-        //Strings are immutable
+    public static void printletter(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+    }   
+    public static void main(String args[]){
+        // int arr[]={'a','b','c'};
+        // String str ="abcd";
+        // String str2= new String("xyz");
+
+        // //Strings are immutable
         Scanner sc = new Scanner(System.in);
-        String name;
-        String line;
-        name = sc.next();
-        line = sc.nextLine();
-        System.out.println("Your name is " + name);
-        System.out.println(line);
+        // String name;
+        // String line;
+        // name = sc.next();
+        // line = sc.nextLine();
+        // System.out.println("Your name is " + name);
+        // System.out.println(line);
         
-        //Length
-        System.out.print(line.length());
+        // //Length
+        // System.out.print(line.length());
         
-        //contatention
-        String First_name = sc.next();
-        String last_name = sc.next();
-        System.out.println(First_name+" "+last_name);
+        // //contatention
+        // String First_name = sc.next();
+        // String last_name = sc.next();
+        // System.out.println(First_name+" "+last_name);
+        String str = sc.nextLine();
+        printletter(str);
         sc.close(); 
     }
 }
