33eee0caeb78518c58075a9c809ee5e5439e6231
Rohit Deore <deorerohit2004@gmail.com>
2026-03-24 19:40:07 +0530

    Done with the Bitwise Operator

--- Bit Manipulation/And_operator.java
+++ Bit Manipulation/And_operator.java
@@ -6,5 +6,14 @@ public class And_operator {
         System.out.println((5| 6));
         // Binaray XOR
         System.out.println((5 ^ 6));
+
+        // Binaray One's Compliment
+        System.out.println(~5);
+        System.out.println(~0);
+
+        //Binaray left shift
+        System.out.println(5<<2);
+        //Binaray Right shift
+        System.out.print(6>>1);
     }    
 }
