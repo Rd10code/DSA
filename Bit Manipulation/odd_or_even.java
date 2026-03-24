12739680a208e312753c04bf13ea8a2c2a264377
Rohit Deore <deorerohit2004@gmail.com>
2026-03-24 20:33:19 +0530

    Updated the Odd Even game

--- Bit Manipulation/odd_or_even.java
+++ Bit Manipulation/odd_or_even.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class odd_or_even {
 
     public static void OddorEven(int n){
@@ -9,9 +10,29 @@ public class odd_or_even {
         }
     }
     public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = Scan.nextInt();
+
+        OddorEven(num);
         OddorEven(2);
         OddorEven(11);
         OddorEven(113);
+
+        while (true) {
+            System.out.println("Enter the number or If You wanna Quit Type Quit");
+            String input = Scan.next();
+
+            if(input.equalsIgnoreCase("quit")){
+                System.out.println("Quitting the game...");
+                break;
+            }
+
+            int number = Integer.parseInt(input);
+            OddorEven(number);
+        }
+        Scan.close();
         
     }
+    
 }
