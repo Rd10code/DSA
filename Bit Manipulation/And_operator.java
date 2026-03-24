12d9b97de39e3928aa50626567faff0b95c3f5bc
Rohit Deore <deorerohit2004@gmail.com>
2026-03-24 11:15:58 +0530

    STarted bit manipulation

---
+++ Bit Manipulation/And_operator.java
@@ -0,0 +1,10 @@
+public class And_operator {
+    public static void main(String[] args) {
+        // Binaray AND
+        System.out.println((5&6));
+        // bInary OR
+        System.out.println((5| 6));
+        // Binaray XOR
+        System.out.println((5 ^ 6));
+    }    
+}
