4263d5906240638e774ae8650431373b6c7cce8e
Rohit Deore <deorerohit2004@gmail.com>
2026-03-25 00:28:59 +0530

    Done withget Ith bit

--- Bit Manipulation/BIts_operation.java
+++ Bit Manipulation/BIts_operation.java
@@ -1,14 +1,14 @@
 public class BIts_operation{
-
-    public static void GetIthbit(int n ,int i){
+    public static void GetIthbit(int n, int i){
         int bitmask = 1<<i;
         if((n & bitmask)==0){
             System.out.print("0");
         }else{
             System.out.print("1");
         }
+
     }
     public static void main(String[] args) {
-        GetIthbit(2, 0);
-    }   
+        GetIthbit(2,1);
+    }
 }
