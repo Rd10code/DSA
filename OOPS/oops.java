4ab67d7395157f3a5236e0c8b45e979474dbea06
Rohit Deore <deorerohit2004@gmail.com>
2026-03-26 00:27:42 +0530

    classes and objects learn

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -1,5 +1,37 @@
-package OOPS;
+public class OOPS {
+    public static void main(String args[]){
+        Pen p1 = new Pen();
+        p1.setColor("yellow");
+        System.out.println(p1.color);
+        p1.settip(6);
+        System.out.println(p1.tip);
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
+}
 
-public class oops {
-    
+class Pen{
+    // properties
+
+    String color ;
+    int tip;
+
+    void setColor(String newcolor){
+        color = newcolor;
+    }
+
+    void settip(int newtip){
+        tip = newtip;
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    float percentage;
+
+    void Calper(int math,int phy , int chem){
+        percentage = math+phy+chem/3;
+    }
+
 }
