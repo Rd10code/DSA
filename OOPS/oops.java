899b10bad91b127b97088d48083790184b919abd
Rohit Deore <deorerohit2004@gmail.com>
2026-03-26 01:44:39 +0530

    Done with the Encapsulation

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -2,19 +2,51 @@ public class OOPS {
     public static void main(String args[]){
         Pen p1 = new Pen();
         p1.setColor("yellow");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.settip(6);
-        System.out.println(p1.tip);
+        System.out.println(p1.gettip());
         p1.setColor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
+        p1.setColor("Black");
+        System.out.println(p1.getColor());
+
+        bankaccount Myaccount = new bankaccount();
+        Myaccount.username ="Rohit";
+        System.out.println("Your username is " + Myaccount.username);
+        // Myaccount.password ="ABCD";
+        Myaccount.setpasword("ABCD");
+        Myaccount.Getpass();
     }
 }
 
+class bankaccount{
+    public String username;
+    private String password;
+    public void setpasword(String pwd){
+        password = pwd;
+    }
+
+    public String Getpass(){
+        System.out.print(password);
+        return password;
+    }
+
+
+
+}
 class Pen{
     // properties
 
-    String color ;
-    int tip;
+    private String color ;
+    private int tip;
+
+    String getColor(){
+        return this.color;
+    }
+
+    int gettip(){
+        return this.tip;
+    }
 
     void setColor(String newcolor){
         color = newcolor;
@@ -31,7 +63,7 @@ class Student{
     float percentage;
 
     void Calper(int math,int phy , int chem){
-        percentage = math+phy+chem/3;
+        percentage = (math+phy+chem)/3;
     }
 
 }
