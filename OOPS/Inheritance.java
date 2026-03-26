64648785e313851eac4de103eeb45ef30aee4857
Rohit Deore <deorerohit2004@gmail.com>
2026-03-26 19:23:04 +0530

    Done with Types of Inheritance

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -1,7 +1,21 @@
 public class Inheritance {
     public static void main(String[] args) {
+
+        // Single and Multilevel Inheritance
+        
         Fish tuna = new Fish();
         tuna.eat();
+        dogs dobby = new dogs();
+        dobby.eat();
+        dobby.legs = 4;
+        dobby.breed = "husky";
+        System.out.println(dobby.legs);
+        System.out.println(dobby.breed);
+
+        // You can Also Try Hirachical Inheritance
+
+
+
     }
 }
 
@@ -17,15 +31,45 @@ class Animal{
     }
 }
 
-// Child Class or Derived Class
-class Fish extends Animal{
-    int fins;
-    void swim(){
-        System.out.println("Swim");
-    }
-
-    void predict(){
-        System.out.println("it is the water predictor");
-    }
+// Single level and multilevel Inheritance
+class Mammal extends Animal{
+  int legs;
+   void walk(){
+    System.out.print("walk");
+  }
     
 }
+class dogs extends Mammal{
+    String breed;
+}
+
+
+// Hirachial Inheritance
+
+class Fish extends Animal{
+  void Swim(){
+    System.out.print("Swim");
+  }
+}
+
+class Bird extends Animal{
+  void Fly(){
+    System.out.print("Fly");
+  }
+}
+
+   
+
+
+// Child Class or Derived Class
+// class Fish extends Animal{
+//     int fins;
+//     void swim(){
+//         System.out.println("Swim");
+//     }
+
+//     void predict(){
+//         System.out.println("it is the water predictor");
+//     }
+    
+// }
