d2136ed39e3fe30fcb4432c420111a4a056c926b
Rohit Deore <deorerohit2004@gmail.com>
2026-04-05 19:26:25 +0530

    Done with both type of factorial

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -1,18 +1,33 @@
 public class factorial {
-    public static int fact(int n){
-        if(n==0){
-            return 1;
-        }
-        
-        int FN_1 = fact(n-1);
-        int FN = FN_1 * n;
-        return FN;
 
+    //normal method
 
+    public static int norfact(int n){
+        int fact = 1;
+     for(int i=1;i<=n;i++){
+        fact = fact*i;
+     }
+     return fact;
     }
+
+    //Recursion
+    // public static int fact(int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+        
+    //     int FN_1 = fact(n-1);
+    //     int FN = FN_1 * n;
+    //     return FN;
+
+
+    // }
     public static void main(String[] args) {
-        int ans = fact(6);
-        System.out.print(ans);
+        // int ans = fact(6);
+        // System.out.print(ans);
+
+        int ans =norfact(5);
+        System.out.println( ans) ;
         
     }
 }
