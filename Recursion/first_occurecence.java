95cbbe7f0c0ac95c72b0b860651ab82ae3ae2ffc
Rohit Deore <deorerohit2004@gmail.com>
2026-04-07 20:14:11 +0530

    Start  with the last occurance

--- Recursion/first_occurecence.java
+++ Recursion/first_occurecence.java
@@ -1,5 +1,5 @@
 public class first_occurecence {
-    public static int fioccur(int arr[],int key,int i){
+     public static int fioccur(int arr[],int key,int i){
         if(key==arr.length){
             return -1;
         }
@@ -12,8 +12,8 @@ public class first_occurecence {
     public static void main(String[] args) {
         int arr[]={1,3,2,4,6,7,8,9,4};
 
-        System.out.print("Your Element on ");
+        // System.out.print("Your Element on ");
         System.out.print(fioccur(arr, 4, 0));
-        System.out.print(" index");
+        // System.out.print(" index");
     }
 }
---
+++ Recursion/last_occurance.java
@@ -0,0 +1,12 @@
+public class last_occurance{
+
+     public static int Lastccur(int arr[],int key,int i){
+
+        
+
+        return i;
+        }
+        public static void main(String args[]){
+       
+        }
+}
