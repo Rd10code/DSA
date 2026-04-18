5c9ca828bc5c410a9c81f59d07ee4a9ed9e0dfb3
Rohit Deore <deorerohit2004@gmail.com>
2026-04-18 12:45:19 +0530

    Done with 5 Patterns

--- Striver patterns/first_pattern.java
+++ Striver patterns/first_pattern.java
@@ -9,13 +9,63 @@ public class first_pattern{
         System.out.println();
     }
    }
-    public static void main(String args[]){
+
+   public static void patter2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+   }
+   
+   public static void pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(j +" ");
+        }
+        System.out.println();
+    }
+   }
+  
+
+   public static void pattern4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+   }
+   
+   public static void pattern5(int n){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+   }
+
+//     public static void pattern5(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int j=0;j<=n-i+1;j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//    }
+
+// Done by the Striver
+
+   
+   public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         while(t>0){
             int n = sc.nextInt();
-            patter1(n);
+            pattern5(n);
             t--;
         }
 
